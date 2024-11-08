9ecafa0ff6468a4e5a28622cefaff184462b44a1
adityahazarika <46670592+adityahazarika@users.noreply.github.com>
2024-11-08 17:34:29 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -75,7 +75,7 @@ function fun(){
     let right = arr.length - 1;
 
     while (left <= right) {
-        let mid = Math.floor(right - left / 2);
+        let mid = Math.floor(right + left / 2);
         if (arr[mid] == num) {
             return mid
         }
@@ -118,3 +118,31 @@ Explanation: The subarray [4,-1,2,1] has the largest sum 6.
 }
 console.log(maxSubArray([-1,-2]))
 console.log(maxSubArray([8,-19,5,-4,20]))
+
+
+
+Q5. implementation of a binary search on a 2D sorted matrix that has a time complexity of O(log(m * n))
+
+let matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+
+var searchMatrix = function(matrix, target) {
+    let left = 0;
+    let right = (matrix.length*[matrix[0].length])-1;
+
+    while(left<=right){
+        let mid = Math.floor((right+left)/2);
+        let num = matrix[Math.floor(mid/matrix[0].length)][mid%matrix[0].length];
+
+        if(num==target){
+            return true
+        }
+        else if(target<num){
+            right = mid-1;
+        }else{
+            left = mid+1
+        }
+    }
+    return false
+};
+
+console.log(searchMatrix(matrix,12))
