0ad1fd28ec749db14e8bda9c148486ac80fd0ea4
adityahazarika <46670592+adityahazarika@users.noreply.github.com>
2024-11-15 16:48:22 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -316,3 +316,37 @@ leetcode solution, although i have already thought this solution but i don't kno
     }
     return prev;
 };
+
+Q10- Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+
+The test cases are generated so that the length of the output will never exceed 105.
+
+function decodeString(s){
+    
+    let stk = [];
+    for (const str of s) {
+        if(str!=="]"){
+            stk.push(str);
+            continue;
+        }
+        let p = stk.pop();
+        let sub = "";
+        while(p!="["){
+            sub = p + sub;
+            p = stk.pop()
+        }        
+        p = stk.pop();
+        let num = "";
+        while(!Number.isNaN(Number(p))){
+            num = p+num;
+            p = stk.pop();
+        }
+        stk.push(p)
+        stk.push(sub.repeat(Number(num)))
+    }
+    return stk.join("")
+}
