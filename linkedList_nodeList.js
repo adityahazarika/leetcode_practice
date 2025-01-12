b04b9a4556a2884e41ca162b1beb11eecce41680
adityahazarika <46670592+adityahazarika@users.noreply.github.com>
2025-01-12 23:12:09 +0530

    Update linkedList_nodeList.js

--- linkedList_nodeList.js
+++ linkedList_nodeList.js
@@ -21,3 +21,53 @@ function ListNode(val, next) {
 
 let num1 = new ListNode(1,new ListNode(2))
 console.log(num1)
+
+//best way i think
+
+class LinkedList {
+    head = null;
+    last;
+
+    push(data) {
+        if (!this.head) {
+            this.head = {
+                value: data,
+                next: null
+            }
+            this.last = this.head
+        } else {
+            this.last.next = {
+                value: data,
+                next: null
+            }
+            this.last = this.last.next
+        }
+        return 0
+    }
+
+    insert(data, index) {
+        let res = this.head;
+        while (res.value != index) {
+            res = res.next
+        }
+        let temp = res.next;
+        res.next = {
+            value: data,
+            next: temp
+        }
+    }
+
+    get() {
+        return this.head
+    }
+}
+
+let list = new LinkedList()
+
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+list.push(5);
+list.insert(6,1)
+console.log(list.get())
